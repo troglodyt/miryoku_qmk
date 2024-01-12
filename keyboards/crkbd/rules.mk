4ae20583a2994977ebc87562ba3e36e5f9679a2e
Christoph Werle <christoph.werle@mercedes-benz.com>
2024-01-12 11:28:46 +0100

    bring backlight back; oled settings preparation

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -7,12 +7,15 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = WS2812
 LTO_ENABLE = yes
+
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
 
 # if firmware size over limit, try this option
 # LTO_ENABLE = yes
