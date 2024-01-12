4b8207c037aa46b53307cbb04ea02d4552fdb97a
Christoph Werle <christoph.werle@mercedes-benz.com>
2024-01-12 12:11:01 +0100

    capitalization for oled

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -15,7 +15,7 @@ RGB_MATRIX_DRIVER = WS2812
 LTO_ENABLE = yes
 
 OLED_ENABLE = yes
-OLED_DRIVER = ssd1306
+OLED_DRIVER = SSD1306
 
 # if firmware size over limit, try this option
 # LTO_ENABLE = yes
